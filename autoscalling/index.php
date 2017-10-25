20ef62b1d7d53ca3c2f09e8ec3649fd7331d56e7
FeijaoUK <feijaouk@users.noreply.github.com>
2017-10-25 15:48:45 +0100

    Update index.php

--- autoscalling/index.php
+++ autoscalling/index.php
@@ -39,11 +39,12 @@
 <style>
 
 body {
-	background: url(https://raw.githubusercontent.com/feijaouk/demos123.net/master/autoscalling/images/<?php echo $selectedBg; ?>) no-repeat;
+  background: url(https://raw.githubusercontent.com/feijaouk/demos123.net/master/autoscalling/images/<?php echo $selectedBg; ?>) no-repeat;
   background-repeat: no-repeat;
   background-attachment: fixed;
-  background-position: center;
-	margin:0;
+  background-position: right;
+  margin:0;
+  background-size: 120px 60px;
 }
 
 .navbar {
