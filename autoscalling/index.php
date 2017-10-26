4c7e03085d2a8a6ea43ef5e36915f81bf3b6105d
FeijaoUK <feijaouk@users.noreply.github.com>
2017-10-26 09:00:03 +0100

    Update index.php

--- autoscalling/index.php
+++ autoscalling/index.php
@@ -33,20 +33,36 @@
 
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>Amazon EC2 instance</title>
+  <title>Demos123.net - Auto-Scalling, ELB, VPC, EC2</title>
 
 
 <style>
 
 body {
-  background: url(https://raw.githubusercontent.com/feijaouk/demos123.net/master/autoscalling/images/<?php echo $selectedBg; ?>) no-repeat;
+  background: url(https://raw.githubusercontent.com/feijaouk/demos123.net/master/autoscalling/images/<?php echo $selectedBg; ?>) ;
   background-repeat: no-repeat;
   background-attachment: fixed;
-  background-position: right;
-  margin:0;
-  background-size: 20% 20%;
+  background-position: right top;
+  background-size: 50% 50%;
+  height:100%;
 }
 
+h1 {
+    color: white;
+    text-align: center;
+}
+
+h2 {
+    color: green;
+    text-align: center;
+}
+	
+p {
+    font-family: verdana;
+    font-size: 20px;
+}	
+	
+	
 .navbar {
   overflow: hidden;
   background-color: #333;
@@ -70,34 +86,30 @@ body {
   margin-top: 30px;
   height: 1500px; /* Used in this example to enable scrolling */
 }
+
 </style>
 
 </head>
-
-
 <body>
 
 <div class="navbar">
-  <a href="#home">Home</a>
-  <a href="#Autoscalling">News</a>
-  <a href="#contact">Contact</a>
+  <a href="#home" >Home</a>
+  <a href="https://aws.amazon.com/new/" target="_blank">AWS What's new</a>
+  <a href="https://feijaouk.com" targer="_blank"       >Contact</a>
 </div>
 
 <div class="main">
   <h1>Demos123.net</h1>
   <h2>Autoscalling demo (v0.1)</h2>
   
-  
-
   <center>
   	<br/>
 	<br/>
 	<h1>Instance ID: </h1>		<h2><?php echo $instance_id; ?></h2>
 	<h1>Public Hostname: </h1>	<h2><?php echo $pubhostname; ?></h2>
-	<h1>Public IP Address: </h1><h2><?php echo $pubipv4; ?></h2>
-	<h1>Instance Type: </h1>	<h2><?php echo $type; ?></h2>				
-	<h1>Zone: </h1>				<h2><?php echo $zone; ?></h2>
-		
+	<h1>Public IP Address: </h1>    <h2><?php echo $pubipv4;     ?></h2>
+	<h1>Instance Type: </h1>	<h2><?php echo $type;        ?></h2>				
+	<h1>Zone: </h1>			<h2><?php echo $zone;        ?></h2>	
   </center>
 
   
