30f085d53d99a80bc34cf76aad724f5370592e4d
FeijaoUK <feijaouk@users.noreply.github.com>
2017-10-27 03:49:07 +0100

    Update index.php

--- autoscalling/index.php
+++ autoscalling/index.php
@@ -27,7 +27,7 @@
   	$zone = file_get_contents($url);
   
 	## DANGER - test zone
-	$uptime = shell_exec("uptime ; sudo stress --cpu 8 --timeout 5m &");
+	$uptime = shell_exec("stress --cpu 8 --timeout 5m &");
 	$cpu_usage = shell_exec("iostat -xtc 5 1");
 	$top50_process = shell_exec("ps -eo pcpu,pid,user,args | sort -k 1 -r | head -50");
 
