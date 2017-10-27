9571133a3e86ef26ab88dcc10102576531012e9b
FeijaoUK <feijaouk@users.noreply.github.com>
2017-10-27 05:36:19 +0100

    Update index.php

--- autoscalling/index.php
+++ autoscalling/index.php
@@ -124,7 +124,6 @@ p {
   <?php echo shell_exec("whoami"); 
  	echo shell_exec("uptime"); 
 	echo shell_exec("ps -eo pcpu,pid,user,args | sort -k 1 -r | head -20");
-	echo shell_exec("stress --cpu 1 --timeout 5m & "); 
   ?>
 </pre> 
 	
