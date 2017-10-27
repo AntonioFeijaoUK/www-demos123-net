b9da8955a7110e74f49fd6e96aa6dc25d65d9a6b
FeijaoUK <feijaouk@users.noreply.github.com>
2017-10-27 05:36:33 +0100

    Update stress-on.php

--- autoscalling/stress-on.php
+++ autoscalling/stress-on.php
@@ -2,6 +2,6 @@
 <?php echo shell_exec("whoami"); 
  	echo shell_exec("uptime"); 
 	echo shell_exec("ps -eo pcpu,pid,user,args | sort -k 1 -r | head -20");
-	echo shell_exec("killall stress"); 
+	echo shell_exec("stress --cpu 1 --timeout 5m & "); 
 ?>
 </pre>
