b3ecf3af82d399f7d1c6635431b99dc5da2c5285
FeijaoUK <feijaouk@users.noreply.github.com>
2017-10-30 16:25:14 +0000

    Update stress-off.php

--- autoscalling/stress-off.php
+++ autoscalling/stress-off.php
@@ -2,6 +2,7 @@
 <?php echo shell_exec("whoami"); 
  	echo shell_exec("uptime"); 
 	echo shell_exec("ps -eo pcpu,pid,user,args | sort -k 1 -r | head -20");
-	echo shell_exec("killall stress"); 
+	echo shell_exec("killall stress");
+	echo "<h2>Linux stress process <b>killall stress</b> command sent!</h2>";
 ?>
 </pre>
