188bee5c5065b82b4bd60cf27b3a698300d36688
FeijaoUK <feijaouk@users.noreply.github.com>
2018-03-12 16:00:39 +0000

    Update index.php

--- secure/index.php
+++ secure/index.php
@@ -38,7 +38,7 @@
       </div>
       <div class="navbar navbar-dark bg-dark box-shadow">
         <div class="container d-flex justify-content-between">
-          <a href="https://aws.amazon.com/certificate-manager/" targer="_blank" class="navbar-brand d-flex align-items-center"><strong>Demo on AWS Certificate Manager</strong></a>
+          <a href="https://aws.amazon.com/certificate-manager/" target="_blank" class="navbar-brand d-flex align-items-center"><strong>Demo on AWS Certificate Manager</strong></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
