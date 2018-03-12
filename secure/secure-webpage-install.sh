861c5380ec11a5049cb472df2fe2abbb09fb4e36
FeijaoUK <feijaouk@users.noreply.github.com>
2018-03-12 13:43:46 +0000

    Update secure-webpage-install.sh

--- secure/secure-webpage-install.sh
+++ secure/secure-webpage-install.sh
@@ -3,9 +3,10 @@
 ## website: https://security.demos123.net
 ##
 ## Demos123.net - Testing https://console.aws.amazon.com/acm/home?region=us-east-1#/
-## AWS Certificate Manager
+## Demo on AWS Certificate Manager
 ##
 ##  @FeijaoUK
+##  Date: 2018-03-12
 ##
 
 ## Selecting working directory
@@ -15,24 +16,20 @@ cd /tmp
 yum remove -y httpd* php*
 
 ## updating OS
-#yum update -y //remove before going online
+yum -y update
 
 
 ## **INSTALL** specific version of apache and php
-yum install -y httpd jq
+yum -y install httpd jq php
 
 
 ## Download index.html file
 cd /var/www/html
 wget https://raw.githubusercontent.com/feijaouk/demos123.net/master/secure/index.html
 
-## Getting EC2 metadata
-EC2_DOCUMENT=`curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document`
-
-echo -e "This machine was built on $(date) /n
-script version control 2017-10-20-1709 /n
-${EC2_DOCUMENT} " > version.html
 
 ## Turn website on on start and start web server now
 systemctl enable httpd.service 
 systemctl start httpd.service 
+
+reboot
